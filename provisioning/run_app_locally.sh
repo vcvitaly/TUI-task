4746c61f7d8997e5489b20c69d3b6d3be3c27e09
vitalii-chura-axon <vchura@axon.dev>
2022-10-22 21:03:51 +0300

    use local profile for running docker image locally

--- provisioning/run_app_locally.sh
+++ provisioning/run_app_locally.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -itd -p 8080:8080 \
-                -e APPLICATION_PROFILE=prod \
+                -e APPLICATION_PROFILE=local \
                 -e GITHUB_TOKEN=your_token \
                 --name tui_task \
                 tui_task:0.0.1
--- src/test/java/com/github/vcvitaly/tuitask/TuiTaskApplicationTests.java
+++ src/test/java/com/github/vcvitaly/tuitask/TuiTaskApplicationTests.java
@@ -1,10 +1,17 @@
 package com.github.vcvitaly.tuitask;
 
 import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GitHub;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class TuiTaskApplicationTests {
+
+	@MockBean
+	private GitHub gitHub;
 
 	@Test
 	void contextLoads() {
