04f7b62ff0b55a49bdb94c8565088ca83e1a0239
vitalii-chura-axon <vchura@axon.dev>
2022-10-23 21:09:28 +0300

    remove sensitive data from the env.sh

--- provisioning/env.sh
+++ provisioning/env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export ECR_REGISTRY=082373558123.dkr.ecr.eu-west-2.amazonaws.com
+export ECR_REGISTRY=<registry>
 export AWS_REGION=eu-west-2
 export RESOURCE_PREFIX=tui-task
 export IMAGE_NAME=tui_task_0.0.1
-export GITHUB_TOKEN=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
+export GITHUB_TOKEN=<token>
 export APPLICATION_PROFILE=prod
